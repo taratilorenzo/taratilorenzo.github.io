3c82657bdc9381d78e73d29034f3f3cce8018a90
Lorenzo Tarati <tarati.lorenzo@gmail.com>
2022-04-29 02:19:46 +1200

    update

--- js/script.js
+++ js/script.js
@@ -1,8 +1,8 @@
 //using querySelector
-const yeartodate = document.querySelector('#yeartodate');
+const yearToDate = document.querySelector('#yeartodate');
 const option1 = {year: 'numeric'};
-yeartodate.textContent = new Date().toLocaleDateString('en-UK', option1)
+yearToDate.textContent = new Date().toLocaleDateString('en-UK', option1)
 
-const todaysdate = document.getElementById('todaysdate');
+const todaysDate = document.getElementById('todaysdate');
 const option2 = {weekday: 'long', day: 'numeric', month: 'long', year: 'numeric'};
-todaysdate.textContent = new Date().toLocaleDateString('en-UK', option2)
+todaysDate.textContent = new Date().toLocaleDateString('en-UK', option2)
