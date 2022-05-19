7f17b8c4e3302c3f098c02bab5560ddb1ec0d706
Lorenzo Tarati Account <tarati.lorenzo@gmail.com>
2022-05-19 20:15:30 +1200

    chmaber update

--- chamber/js/scripts.js
+++ chamber/js/scripts.js
@@ -1,16 +1,19 @@
 //using querySelector
-const yearToDate = document.querySelector('#yeartodate');
-const option1 = {year: 'numeric'};
-yearToDate.textContent = new Date().toLocaleDateString('en-UK', option1)
+const todaysDate = document.getElementById("currentdate");
+const option1 = {weekday: "long", day: "numeric", month: "long", year: "numeric"};
+todaysDate.textContent = new Date().toLocaleDateString('en-UK', option1)
 
-const todaysDate = document.getElementById('todaysdate');
-const option2 = {weekday: 'long', day: 'numeric', month: 'long', year: 'numeric'};
-todaysDate.textContent = new Date().toLocaleDateString('en-UK', option2)
+const yearToDate = document.querySelector("#yeartodate");
+const option2 = {year: 'numeric'};
+yearToDate.textContent = new Date().toLocaleDateString('en-UK', option2)
+
+const lastUpdate = document.querySelector("#lastmodifieddate");
+lastUpdate.textContent = document.lastModified
 
 function toggleMenu() {
-    document.getElementById("primaryNav").classList.toggle("open");
-    document.getElementById("hamburgerBtn").classList.toggle("open");
+    document.querySelector("#primary-nav").classList.toggle("open");
+    document.querySelector("#hamburger-btn").classList.toggle("open");
 }
 
-const x = document.getElementById("hamburgerBtn")
+const x = document.querySelector("#hamburger-btn")
 x.onclick = toggleMenu;
