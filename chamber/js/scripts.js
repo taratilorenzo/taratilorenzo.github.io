9ce66e7fd6fe7bad77524677e1037364c1773baa
Lorenzo Tarati Account <tarati.lorenzo@gmail.com>
2022-05-21 01:59:50 +1200

    updated chamber css

--- chamber/js/scripts.js
+++ chamber/js/scripts.js
@@ -17,3 +17,13 @@ function toggleMenu() {
 
 const x = document.querySelector("#hamburger-btn")
 x.onclick = toggleMenu;
+
+// title of the page changes based on page you are on
+const navSelected = document.querySelectorAll("#primary-nav a");
+const pageTitle = document.querySelector(".page-title");
+
+for(let i=0; i<navSelected.length; i++){
+    navSelected[i].addEventListener("click", ()=>{
+        pageTitle.innerHTML = navSelected[i].textContent.toUpperCase();
+    });
+}
