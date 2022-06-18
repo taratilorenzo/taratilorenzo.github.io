26de55c3624eae5e25f45919886f4cd9d4503134
Lorenzo Tarati Account <tarati.lorenzo@gmail.com>
2022-06-18 22:23:09 +1200

    update

--- chamber/js/visits.js
+++ chamber/js/visits.js
@@ -1,5 +1,6 @@
 // initialize display
 const visitDisplay = document.querySelector(".visits");
+const lastDateDisplay = document.querySelector(".last-date-visit")
 
 // get the stored value from the local storage
 let numVisits = Number(window.localStorage.getItem("visits-ls"));
@@ -8,7 +9,7 @@ let numVisits = Number(window.localStorage.getItem("visits-ls"));
 if (numVisits !== 0) {
     visitDisplay.textContent = numVisits;
 } else {
-    visitDisplay.textContent = "This is your first visit";
+    visitDisplay.textContent = `This is your first visit`;
 }
 
 // incrementing numVisits
@@ -16,3 +17,30 @@ numVisits++;
 
 // store in the local storage the number of visits
 localStorage.setItem("visits-ls", numVisits);
+
+// Using localStorage to display how many days it has been since the user's last visit to your site. 
+
+// get the last visit date value stored in localStorage
+
+let lastVisitDate = Date.now(window.localStorage.getItem("last-date-visit"));
+// console.log(lastVisitDate);
+
+// get todays date - leave the variable todayy with double yy's its not an error,changing it it will cause errors
+const todayy = new Date();
+
+//Using JS calculate how many milliseconds are in a day. 
+const msInDay = 1000 * 60 * 60 * 24;
+
+
+// Creating a variable for today and another variable for the last visit and then subtract and divide by the number of milliseconds. 
+// Then use Math.round() to make it a whole number.
+let diff = todayy - lastVisitDate;
+let dayDifference = Math.round((diff)/msInDay);
+
+
+//Displaying a message about how many days it's been or if this is the first visit.
+document.querySelector(".last-date-visit").textContent = dayDifference;
+
+
+// update the lastVisitDate
+localStorage.setItem("last-date-visit",todayy);
