c5a6f728d83bb46a478ba7a5c0f3c59766e4014a
Lorenzo Tarati Account <tarati.lorenzo@gmail.com>
2022-06-21 23:05:51 +1200

    update weather api in the chamber

--- chamber/js/windchill.js
+++ chamber/js/windchill.js
@@ -19,6 +19,28 @@ const mph = (kmh) => {
 const kmh = (mph) => {
     return mph * 1.609344;
 }
+
+const weatherURL = "//api.openweathermap.org/data/2.5/weather?id=2193732&appid=f469930a2094b212b1b978211a0dffd1&units=imperial"
+
+fetch(weatherURL)
+    .then((response) => response.json())
+    .then((weatherInfo) => {
+        console.log(weatherInfo);
+        document.querySelector(".place").innerHTML = weatherInfo.name;
+        document.querySelector(".temp-celsius").innerHTML = weatherInfo.main.temp;
+        document.querySelector(".wind-speed").innerHTML = weatherInfo.wind.speed;
+
+        const iconCode = weatherInfo.weather[0].icon;
+        const icon_path = "//openweathermap.org/img/w/"+ iconCode +".png";
+
+        document.querySelector(".weather-img").src = icon_path;
+    });
+
+
+
+
+
+
 // console.log(document.querySelector(".temp-celsius").textContent);
 let temp = farenheit(parseFloat(document.querySelector(".temp-celsius").textContent));
 let speed = mph(parseFloat(document.querySelector(".wind-speed").textContent));
